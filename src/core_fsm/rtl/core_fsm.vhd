a065fdeb08cddaf6c87e97ff436b80a89ca2e12b
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2020-02-07 15:44:13 +0100

    Implement basic read/write for core_fsm module

--- src/core_fsm/rtl/core_fsm.vhd
+++ src/core_fsm/rtl/core_fsm.vhd
@@ -1,54 +1,134 @@
 library xil_defaultlib;
 library IEEE;
-use IEEE.std_logic_1164.ALL;
+use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 use xil_defaultlib.global.all;
 
 entity core_fsm is
     port (
-        clk            : in std_logic;
-        resetn         : in std_logic;
-        clk_fs         : in std_logic;
-        soft_reset     : in std_logic;
-        system_enable  : in std_logic;
-        system_running : in std_logic;
+        clk            : in  std_logic;
+        resetn         : in  std_logic;
+        clk_fs         : in  std_logic;
+        soft_reset     : in  std_logic;
+        system_enable  : in  std_logic;
+        system_running : out std_logic;
 
-        -- Left channel
-        conv_op_lch        : in  std_logic;
-        conv_req_lch       : out conversion_req_t;
-        conv_rsp_lch       : in  conversion_rsp_t;
-        internal_error_lch : in  std_logic;
+        conv_op        : in  std_logic;
+        conv_req       : out conversion_req_t;
+        conv_rsp       : in  conversion_rsp_t;
+        internal_error : in  std_logic;
 
-        pattern_req_lch      : out std_logic;
-        pattern_len_lch      : out unsigned(9 downto 0);
-        pattern_finished_lch : in  std_logic;
-        pattern_tlast_lch    : in  std_logic;
+        pattern_req      : out std_logic;
+        pattern_len      : out unsigned(9 downto 0);
+        pattern_finished : in  std_logic;
+        pattern_tlast    : in  std_logic;
 
-        buffer_size_l : in unsigned(10 downto 0);
-        bram_ptr_l    : in std_logic_vector(31 downto 0);
-
-        -- Right channel
-        conv_op_rch        : in  std_logic;
-        conv_req_rch       : out conversion_req_t;
-        conv_rsp_rch       : in  conversion_rsp_t;
-        internal_error_rch : in  std_logic;
-
-        pattern_req_rch      : out std_logic;
-        pattern_len_rch      : out unsigned(9 downto 0);
-        pattern_finished_rch : in  std_logic;
-        pattern_tlast_rch    : in  std_logic;
-
-        buffer_size_r : in unsigned(10 downto 0);
-        bram_ptr_r    : in std_logic_vector(31 downto 0)
+        buffer_size : in unsigned(10 downto 0);
+        bram_ptr    : in std_logic_vector(31 downto 0);
+        read_size   : in unsigned(15 downto 0)
         );
-    
+
 end core_fsm;
 
 architecture RTL of core_fsm is
 
+    type fsm_states is (
+        IDLE,
+        REQ_S2MM,
+        WAIT_S2MM,
+        REQ_MM2S,
+        WAIT_MM2S
+        );
+    signal state       : fsm_states;
+
+    signal s2mm_write_ptr : std_logic_vector(31 downto 0);
+    signal mm2s_read_ptr  : std_logic_vector(31 downto 0);
+    signal s2mm_write_req_size : unsigned(9 downto 0);
+    signal mm2s_read_req_size  : unsigned(9 downto 0);
+
+    constant S2MM_WRITE_SIZE : natural := 32;
+    constant MM2S_READ_SIZE  : natural := 32;
 
 begin
 
+
+    fsm_axi_full : process (clk)
+    begin
+        if (rising_edge(clk)) then
+            if (resetn = '0' or soft_reset = '1') then
+                state          <= IDLE;
+                system_running <= '0';
+
+                conv_req            <= (request => '0', op_type => S2MM, size => (others => '0'), address => (others => '0'));
+                s2mm_write_ptr      <= (others  => '0');
+                mm2s_read_ptr       <= (others  => '0');
+                s2mm_write_req_size <= (others  => '0');
+                mm2s_read_req_size  <= (others  => '0');
+
+            else
+                -- Default outputs
+                conv_req.request <= '0';
+                conv_req.size    <= (others => '0');
+                conv_req.address <= (others => '0');
+                system_running   <= '1';
+
+                -- FSM
+                case state is
+                    when IDLE =>
+                        system_running <= '0';
+                        if (system_enable) then
+                            if (conv_op = '0' and buffer_size >= S2MM_WRITE_SIZE) then
+                                state <= REQ_S2MM;
+                            elsif (conv_op = '1' and mm2s_read_ptr(15 downto 0) < std_logic_vector(read_size)) then
+                                state <= REQ_MM2S;
+                            end if;
+                        end if;
+
+
+                    when REQ_S2MM =>
+                        if (buffer_size >= S2MM_WRITE_SIZE) then
+                            conv_req.op_type <= S2MM;
+                            conv_req.request <= '1';
+                            conv_req.size    <= to_unsigned(S2MM_WRITE_SIZE, 10);
+                            conv_req.address <= s2mm_write_ptr;
+
+                            s2mm_write_req_size <= to_unsigned(S2MM_WRITE_SIZE, 10);
+                            state               <= WAIT_S2MM;
+                        end if;
+
+
+                    when WAIT_S2MM =>
+                        if (conv_rsp.s2mm_done) then
+                            s2mm_write_ptr <= std_logic_vector(unsigned(s2mm_write_ptr) + s2mm_write_req_size);
+                            state          <= IDLE;
+                        end if;
+
+
+                    when REQ_MM2S =>
+                        conv_req.op_type <= MM2S;
+                        conv_req.request <= '1';
+                        conv_req.size    <= read_size(9 downto 0);
+                        conv_req.address <= mm2s_read_ptr;
+
+                        mm2s_read_req_size <= read_size(9 downto 0);
+                        state              <= WAIT_MM2S;
+
+
+                    when WAIT_MM2S =>
+                        if (conv_rsp.mm2s_done) then
+                            mm2s_read_ptr <= std_logic_vector(unsigned(mm2s_read_ptr) + mm2s_read_req_size);
+                            state         <= IDLE;
+                        end if;
+
+                end case;
+
+                if (not system_enable) then
+                    state <= IDLE;
+                end if;
+
+            end if;
+        end if;
+    end process fsm_axi_full;
 
 
 end RTL;
--- src/core_fsm/tb/tb_core_fsm.vhd
+++ src/core_fsm/tb/tb_core_fsm.vhd
@@ -1,7 +1,8 @@
 library IEEE;
 library xil_defaultlib;
-use IEEE.std_logic_1164.ALL;
+use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
+use IEEE.math_real.all;
 use xil_defaultlib.global.all;
 use xil_defaultlib.global_sim.all;
 
@@ -10,37 +11,28 @@ end tb_core_fsm;
 
 architecture TB of tb_core_fsm is
 
-    signal clk : std_logic := '0';
-    signal resetn               : std_logic;
-    signal clk_fs               : std_logic;
-    signal soft_reset           : std_logic;
+    signal clk        : std_logic := '0';
+    signal resetn     : std_logic := '0';
+    signal clk_fs     : std_logic := '0';
+    signal soft_reset : std_logic := '0';
 
-    signal system_enable        : std_logic;
-    signal system_running       : std_logic;
+    signal system_enable  : std_logic := '0';
+    signal system_running : std_logic := '0';
 
-    signal conv_op_lch          : std_logic;
-    signal conv_req_lch         : conversion_req_t;
-    signal conv_rsp_lch         : conversion_rsp_t;
-    signal internal_error_lch   : std_logic;
-    signal pattern_req_lch      : std_logic;
-    signal pattern_len_lch      : unsigned(9 downto 0);
-    signal pattern_finished_lch : std_logic;
-    signal pattern_tlast_lch    : std_logic;
-    signal buffer_size_l        : unsigned(10 downto 0);
-    signal bram_ptr_l           : std_logic_vector(31 downto 0);
-
-    signal conv_op_rch          : std_logic;
-    signal conv_req_rch         : conversion_req_t;
-    signal conv_rsp_rch         : conversion_rsp_t;
-    signal internal_error_rch   : std_logic;
-    signal pattern_req_rch      : std_logic;
-    signal pattern_len_rch      : unsigned(9 downto 0);
-    signal pattern_finished_rch : std_logic;
-    signal pattern_tlast_rch    : std_logic;
-    signal buffer_size_r        : unsigned(10 downto 0);
-    signal bram_ptr_r           : std_logic_vector(31 downto 0);
+    signal conv_op          : std_logic             := '0';
+    signal conv_req         : conversion_req_t;
+    signal conv_rsp         : conversion_rsp_t;
+    signal internal_error   : std_logic             := '0';
+    signal pattern_req      : std_logic             := '0';
+    signal pattern_len      : unsigned(9 downto 0);
+    signal pattern_finished : std_logic             := '0';
+    signal pattern_tlast    : std_logic             := '0';
+    signal buffer_size      : unsigned(10 downto 0) := (others => '0');
+    signal bram_ptr         : std_logic_vector(31 downto 0);
+    signal read_size        : unsigned(15 downto 0) := (others => '0');
 
     signal stop_clock : std_logic := '0';
+
 
 begin
 
@@ -48,44 +40,149 @@ begin
     clk <= (not clk and not stop_clock) after AXI_CLK_T/2;
 
 
-    DUT: entity xil_defaultlib.core_fsm
+    DUT : entity xil_defaultlib.core_fsm
         port map (
-            clk                  => clk,
-            resetn               => resetn,
-            clk_fs               => clk_fs,
-            soft_reset           => soft_reset,
-            system_enable        => system_enable,
-            system_running       => system_running,
-            -- Left channel
-            conv_op_lch          => conv_op_lch,
-            conv_req_lch         => conv_req_lch,
-            conv_rsp_lch         => conv_rsp_lch,
-            internal_error_lch   => internal_error_lch,
-            pattern_req_lch      => pattern_req_lch,
-            pattern_len_lch      => pattern_len_lch,
-            pattern_finished_lch => pattern_finished_lch,
-            pattern_tlast_lch    => pattern_tlast_lch,
-            buffer_size_l        => buffer_size_l,
-            bram_ptr_l           => bram_ptr_l,
-            -- Right channel
-            conv_op_rch          => conv_op_rch,
-            conv_req_rch         => conv_req_rch,
-            conv_rsp_rch         => conv_rsp_rch,
-            internal_error_rch   => internal_error_rch,
-            pattern_req_rch      => pattern_req_rch,
-            pattern_len_rch      => pattern_len_rch,
-            pattern_finished_rch => pattern_finished_rch,
-            pattern_tlast_rch    => pattern_tlast_rch,
-            buffer_size_r        => buffer_size_r,
-            bram_ptr_r           => bram_ptr_r
+            clk    => clk,
+            resetn => resetn,
+            clk_fs => clk_fs,
+
+            soft_reset     => soft_reset,
+            system_enable  => system_enable,
+            system_running => system_running,
+
+            conv_op        => conv_op,
+            conv_req       => conv_req,
+            conv_rsp       => conv_rsp,
+            internal_error => internal_error,
+
+            pattern_req      => pattern_req,
+            pattern_len      => pattern_len,
+            pattern_finished => pattern_finished,
+            pattern_tlast    => pattern_tlast,
+
+            buffer_size => buffer_size,
+            bram_ptr    => bram_ptr,
+            read_size   => read_size
             );
 
 
-   main: process
-   begin
-       wait for (10*AXI_CLK_T);
-       end_test_and_stop_clock(stop_clock);
-       
-   end process;
+    main : process
+
+        procedure init_values is
+        begin
+            conv_rsp.s2mm_done <= '0';
+            conv_rsp.mm2s_done <= '0';
+        end procedure init_values;
+
+
+        procedure reset_system is
+        begin
+            wait for (10*AXI_CLK_T);
+            resetn <= '1';
+            wait until rising_edge(clk);
+            wait for (10*AXI_CLK_T);
+        end procedure reset_system;
+
+
+        procedure restart_system (constant OP : conversion_op) is
+        begin
+            wait until rising_edge(clk);
+            system_enable <= '0';
+            wait for (5*AXI_CLK_T);
+            if (OP = S2MM) then
+                conv_op <= '0';
+            else
+                conv_op <= '1';
+            end if;
+            wait for (5*AXI_CLK_T);
+            system_enable <= '1';
+            wait until rising_edge(clk);
+        end procedure restart_system;
+
+
+        procedure s2mm_rx (
+            signal buffer_size : inout unsigned(10 downto 0);
+            constant SIZE      : in    natural
+            ) is
+        begin
+            for i in 1 to SIZE loop
+                buffer_size <= buffer_size + 1;
+                wait for (AXI_CLK_T);
+            end loop;
+        end procedure s2mm_rx;
+
+
+        procedure s2mm_consume (
+            signal buffer_size : inout unsigned(10 downto 0);
+            signal conv_rsp    : out   conversion_rsp_t;
+            constant SIZE      : in    natural
+            ) is
+            constant BURST_LEN     : natural := 32;
+            variable NUM_TRANSFERS : natural := 0;
+        begin
+            NUM_TRANSFERS := integer(ceil(real(SIZE)/real(BURST_LEN)));
+
+            for i in 1 to NUM_TRANSFERS loop
+                for j in 1 to BURST_LEN loop
+                    buffer_size <= buffer_size - 1;
+                    wait for (AXI_CLK_T);
+                end loop;
+                conv_rsp.s2mm_done <= '1';
+                wait until rising_edge(clk);
+                conv_rsp.s2mm_done <= '0';
+            end loop;
+
+        end procedure s2mm_consume;
+
+
+        procedure s2mm (
+            signal conv_rsp : out conversion_rsp_t;
+            constant SIZE   : in  natural
+            ) is
+        begin
+            wait for (5*AXI_CLK_T);
+            s2mm_rx(buffer_size, SIZE);
+            wait for (20*AXI_CLK_T);
+            s2mm_consume(buffer_size, conv_rsp, SIZE);
+            wait for (5*AXI_CLK_T);
+        end procedure s2mm;
+
+
+        procedure mm2s (
+            signal conv_rsp  : out conversion_rsp_t;
+            signal read_size : out unsigned(15 downto 0);
+            constant SIZE    : in  natural
+            ) is
+        begin
+            read_size          <= to_unsigned(SIZE, 16);
+            -- Wait for response
+            wait for (50*AXI_CLK_T);
+            -- Send response
+            conv_rsp.mm2s_done <= '1';
+            wait until rising_edge(clk);
+            conv_rsp.mm2s_done <= '0';
+            wait for (5*AXI_CLK_T);
+        end procedure mm2s;
+
+
+    begin
+
+        -- Init test
+        init_values;
+        reset_system;
+        -- S2MM
+        restart_system(S2MM);
+        s2mm(conv_rsp, 32);
+        s2mm(conv_rsp, 128);
+        -- MM2S
+        restart_system(MM2S);
+        mm2s(conv_rsp, read_size, 32);
+        mm2s(conv_rsp, read_size, 128);
+        -- End of tets
+        wait for (50*AXI_CLK_T);
+        end_test_and_stop_clock(stop_clock);
+
+    end process;
+
 
 end TB;
